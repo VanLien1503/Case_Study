80a1d4e2ca9a1a0dfa4b1a9ab718d38cecda2e51
VanLien1503 <hoanglien89nd@gmail.com>
2020-04-17 20:42:26 +0700

    Tạo Phương Thức Di Chuyển và Check Sức Sống cho nhân vật

--- Figure.js
+++ Figure.js
@@ -14,4 +14,16 @@ function Figure(name, vitality, energy, damage, defense, avoid, exactly, x, y) {
         y = this.y;
         return x, y;
     };
+    // tạo phương thức di chuyển
+    this.moveTo = function (nhanvat) {
+        nhanvat.viTriFirst(this.x, this.y);
+        console.log("Di chuyển Đến " + nhanvat.name + " Vị trí " + nhanvat.viTriFirst(this.x, this.y));
+    };
+    //tạo phương thức check sức sống
+    this.daeth = function (nhanvat) {
+        if (nhanvat.vitality <= 0) {
+            alert(nhanvat.name + " The daeth...!");
+            return true;
+        }
+    }
 }
